68c70a5836eec115d080caf59ddf8b80260eb36d
drblah <theblahblah2001@gmail.com>
2022-04-11 16:25:56 +0200

    Add l2 test script
    This script sets up the following topology:
    
    +------------------+                        +------------------+
    |                  |                        |                  |
    |                  |      +--------+        |                  |
    |                  |      |        |        |                  |
    |      Host1       |-----mptun_bridge-------|      Host2       |
    |                  |      |        |        |                  |
    |                  |      +--------+        |                  |
    |                  |                        |                  |
    +------------------+                        +------------------+
              |                                          |
         +----|---+                                 +----|---+
         |        |                                 |        |
         bridge_gw1                                 bridge_gw2
         |        |                                 |        |
         +--------+                                 +--------+
              |                                          |
    +---------|--------+                       +---------|--------+
    |                  |                       |                  |
    |                  |                       |                  |
    |                  |                       |                  |
    |     Client1      |                       |     Client2      |
    |                  |                       |                  |
    |                  |                       |                  |
    |                  |                       |                  |
    +------------------+                       +------------------+
    
    Where Host1, Host2, Client1 and Client2 are network namaspaces
    connected with virtual ethernet and bridge devices.

--- test_tools/l2-many-to-one/setup.sh
+++ test_tools/l2-many-to-one/setup.sh
@@ -16,12 +16,19 @@ trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 ip netns add host1
 ip netns add host2
 
-# Create two veth pairs
+ip netns add client1
+ip netns add client2
+
+# Create veth pairs
 ip link add veth1 type veth peer name br-veth1
 ip link add veth2 type veth peer name br-veth2
 
 ip link add gw_veth1 type veth peer name br-gw-veth1
 ip link add gw_veth2 type veth peer name br-gw-veth2
+
+# Client pairs
+ip link add cli_veth1 type veth peer name br-cl-veth1
+ip link add cli_veth2 type veth peer name br-cl-veth2
 
 # Associate the veth pairs with the namespaces
 ip link set veth1 netns host1
@@ -30,6 +37,8 @@ ip link set gw_veth1 netns host1
 ip link set veth2 netns host2
 ip link set gw_veth2 netns host2
 
+ip link set cli_veth1 netns client1
+ip link set cli_veth2 netns client2
 
 # Assign IPs
 ip netns exec host1 \
@@ -43,6 +52,12 @@ ip netns exec host2 \
 
 ip netns exec host2 \
   ip addr add 172.16.210.3/24 dev gw_veth2
+
+ip netns exec client1 \
+  ip add add 172.16.210.10/24 dev cli_veth1
+
+ip netns exec client2 \
+  ip add add 172.16.210.11/24 dev cli_veth2
 
 # Create bridge
 ip link add name mptun_bridge type bridge
@@ -61,6 +76,9 @@ ip link set br-veth2 up
 ip link set br-gw-veth1 up
 ip link set br-gw-veth2 up
 
+ip link set br-cl-veth1 up
+ip link set br-cl-veth2 up
+
 ip netns exec host1 \
   ip link set veth1 up
 
@@ -73,12 +91,21 @@ ip netns exec host2 \
 ip netns exec host2 \
   ip link set gw_veth2 up
 
+ip netns exec client1 \
+  ip link set cli_veth1 up
+
+ip netns exec client2 \
+  ip link set cli_veth2 up
+
 # Add br-veth* to the bridge
 ip link set br-veth1 master mptun_bridge
 ip link set br-veth2 master mptun_bridge
 
 ip link set br-gw-veth1 master bridge_gw1
 ip link set br-gw-veth2 master bridge_gw2
+
+ip link set br-cl-veth1 master bridge_gw1
+ip link set br-cl-veth2 master bridge_gw2
 
 # Assign address to bridge
 ip addr add 172.16.200.1/24 brd + dev mptun_bridge
@@ -100,6 +127,8 @@ wait $P1
 ## Clean up the bridge and namespaces
 ip netns delete host1
 ip netns delete host2
+ip netns delete client1
+ip netns delete client2
 ip link delete dev mptun_bridge
 ip link delete dev bridge_gw1
 ip link delete dev bridge_gw2
