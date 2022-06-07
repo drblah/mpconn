f5c1960377f536b8609ec8ac90abd563d9ca089a
drblah <theblahblah2001@gmail.com>
2022-06-07 16:48:33 +0200

    Convert peer list to hashmap and add last seen time stamp for each peer
    This change is done in or der to keep track of when a peer was last seen.
    This makes it possible to prune stale peers after a set time out.

--- src/peer_list.rs
+++ src/peer_list.rs
@@ -1,34 +1,45 @@
 use std::net::SocketAddr;
+use std::time::SystemTime;
+use std::collections::HashMap;
 
 pub struct PeerList {
-    peers: Vec<SocketAddr>,
+    peers: HashMap<SocketAddr, Peer>
+}
 
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+pub struct Peer {
+    address: SocketAddr,
+    last_seen: SystemTime
 }
 
 impl PeerList {
-    pub fn new(mut peers: Option<Vec<SocketAddr>>) -> Self {
+    pub fn new(peers: Option<Vec<SocketAddr>>) -> Self {
         let mut peer_list = PeerList{
-            peers: Vec::new()
+            peers: HashMap::new()
         };
 
-        if let Some(mut peers) = peers {
-            peer_list.peers.append(&mut peers);
+        if let Some(peers) = peers {
+            for peer in peers {
+                peer_list.peers.insert(peer, Peer{address: peer, last_seen: SystemTime::now() });
+            }
         }
-
 
         peer_list
     }
 
     pub fn add_peer(&mut self, peer: SocketAddr) {
-        if !self.peers.contains(&peer) {
+        if !self.peers.contains_key(&peer) {
             println!("Adding new peer: {:?}", peer);
-            self.peers.push(peer);
+            self.peers.insert(peer, Peer{address: peer, last_seen: SystemTime::now() });
 
             println!("My peer list:\n{:?}", self.peers)
         }
     }
 
     pub fn get_peers(&mut self) -> Vec<SocketAddr> {
-        self.peers.clone()
+        self.peers.values()
+            .cloned()
+            .map(|peer| peer.address)
+            .collect()
     }
 }
