e693d7ecdf564b25072583e17656fc11d6a78299
drblah <theblahblah2001@gmail.com>
2022-06-08 16:54:19 +0200

    Add stale_time_limit to PeerList
    This is a setting for a duration, after which a Peer should be considered stale.
    A function, prune_stale_peers has been added in order to prune peers whose last_seen is
    older than the stale_time_limit

--- src/peer_list.rs
+++ src/peer_list.rs
@@ -1,26 +1,34 @@
-use std::net::SocketAddr;
-use std::time::SystemTime;
 use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::time::{Duration, SystemTime};
 
 pub struct PeerList {
-    peers: HashMap<SocketAddr, Peer>
+    peers: HashMap<SocketAddr, Peer>,
+    stale_time_limit: Duration,
 }
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 pub struct Peer {
     address: SocketAddr,
-    last_seen: SystemTime
+    last_seen: SystemTime,
 }
 
 impl PeerList {
     pub fn new(peers: Option<Vec<SocketAddr>>) -> Self {
-        let mut peer_list = PeerList{
-            peers: HashMap::new()
+        let mut peer_list = PeerList {
+            peers: HashMap::new(),
+            stale_time_limit: Duration::from_secs(10),
         };
 
         if let Some(peers) = peers {
             for peer in peers {
-                peer_list.peers.insert(peer, Peer{address: peer, last_seen: SystemTime::now() });
+                peer_list.peers.insert(
+                    peer,
+                    Peer {
+                        address: peer,
+                        last_seen: SystemTime::now(),
+                    },
+                );
             }
         }
 
@@ -30,16 +38,46 @@ impl PeerList {
     pub fn add_peer(&mut self, peer: SocketAddr) {
         if !self.peers.contains_key(&peer) {
             println!("Adding new peer: {:?}", peer);
-            self.peers.insert(peer, Peer{address: peer, last_seen: SystemTime::now() });
+            self.peers.insert(
+                peer,
+                Peer {
+                    address: peer,
+                    last_seen: SystemTime::now(),
+                },
+            );
 
             println!("My peer list:\n{:?}", self.peers)
+        } else {
+            println!("Updating last_seen for peer: {:?}", peer);
+            self.peers
+                .entry(peer)
+                .and_modify(|peer| peer.last_seen = SystemTime::now());
         }
     }
 
     pub fn get_peers(&mut self) -> Vec<SocketAddr> {
-        self.peers.values()
+        self.peers
+            .values()
             .cloned()
             .map(|peer| peer.address)
             .collect()
+    }
+
+    pub fn prune_stale_peers(&mut self) {
+        let now = SystemTime::now();
+        let old_size = self.peers.len();
+
+
+        self.peers.retain(|_, peer| {
+            now.duration_since(peer.last_seen)
+                .unwrap()
+                .gt(&self.stale_time_limit)
+        });
+
+        let pruned_size = self.peers.len();
+
+        if pruned_size < old_size {
+            println!("Pruned {} stale peers", old_size - pruned_size)
+        }
     }
 }
