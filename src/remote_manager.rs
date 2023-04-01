6934b5832571feef339af373c180ace65d5ba31b
drblah <theblahblah2001@gmail.com>
2023-04-01 19:17:57 +0200

    RemoteManager: Handle error when we cannot keep up with transmitting messages on the Remotes

--- src/remote_manager.rs
+++ src/remote_manager.rs
@@ -1,6 +1,7 @@
 use bytes::Bytes;
 use tokio::select;
 use tokio::sync::{broadcast, mpsc};
+use tokio::sync::broadcast::error::RecvError;
 use tokio::task::JoinHandle;
 use crate::internal_messages::{IncomingPacket, IncomingUnparsedPacket, OutgoingUDPPacket};
 use crate::remote::{AsyncRemote, DecopuledUDPremote, UDPLz4Remote, UDPRemote};
@@ -29,8 +30,16 @@ impl RemoteManager {
                     loop {
                         select! {
                             outgoing = bc.recv() => {
-                                let outgoing = outgoing.unwrap();
-                                remote.write( Bytes::from(outgoing.packet_bytes), outgoing.destination).await
+                                match outgoing {
+                                    Ok(outgoing) => {
+                                        remote.write( Bytes::from(outgoing.packet_bytes), outgoing.destination).await
+                                    }
+                                    Err(e) => {
+                                        eprintln!("RemoteManager: Cannot keep up with sending packets! {}", e);
+                                    }
+                                }
+
+
                             }
 
                             incoming = remote.read2() => {
