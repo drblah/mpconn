dbcffb86ea9a26c790b606499c9f75ad851b5184
drblah <theblahblah2001@gmail.com>
2023-04-24 14:56:19 +0200

    Add peer_id decoding and fix bug in un-zigzag function which results in off by one errors

--- test_tools/Wireshark dissector/mpconn_dissector.lua
+++ test_tools/Wireshark dissector/mpconn_dissector.lua
@@ -3,27 +3,28 @@ local mpconnudp = Proto.new("mpconnudp", "MPCONN UDP")
 -- local field_connid = ProtoField.uint64("mpconnudp.connid", "Conn ID", base.HEX)
 local field_msg = ProtoField.uint8("mpconnudp.msg_type", "Msg type", base.HEX)
 local field_seqnr = ProtoField.uint64("mpconnudp.seqnr", "Seq Nr.", base.DEC)
+local field_peer_id = ProtoField.uint16("mpconnudp.peer_id", "Peer ID", base.DEC)
 local field_msg_length = ProtoField.uint64("mpconnudp.msg_len", "Msg Len", base.DEC)
 local field_ip = ProtoField.bytes("mpconnudp.ip", "IP")
-mpconnudp.fields = {field_msg, field_seqnr, field_msg_length, field_ip}
+mpconnudp.fields = { field_msg, field_seqnr, field_peer_id, field_msg_length, field_ip }
 
 -- Reverse of zigzag: https://docs.rs/bincode/latest/bincode/config/struct.VarintEncoding.html
 function unzigzag(buffer)
     -- Decode u < 251
     if buffer(0, 1):le_uint() < 251 then
-        return buffer(0, 1)
+        return { 1, buffer(0, 1) }
 
     -- Decode 251 <= u < 2**16
     elseif buffer(0, 1):le_uint() == 251 then
-        return buffer(1, 2)
+        return { 3, buffer(1, 2) }
 
     -- Decode 2**16 <= u < 2**32
     elseif buffer(0, 1):le_uint() == 252 then
-        return buffer(1, 4)
+        return { 5, buffer(1, 4) }
 
     -- Decode 2**32 <= u < 2**64
         elseif buffer(0, 1):le_uint() == 253 then
-            return buffer(1, 8)
+        return { 9, buffer(1, 8) }
     end
 
 end
@@ -36,7 +37,7 @@ function mpconnudp.dissector(buffer, pinfo, tree)
     pinfo.cols.protocol = "MPCONN UDP"
 
     -- We label the entire UDP payload as being associated with our protocol
-    local payload_tree = tree:add( mpconnudp, buffer() )
+    local payload_tree = tree:add(mpconnudp, buffer())
 
     local message_type_pos = 0
     local message_type_len = 1
@@ -45,22 +46,26 @@ function mpconnudp.dissector(buffer, pinfo, tree)
 
     local seqnr_pos = message_type_pos + message_type_len
     local seqnr_maxlen = 8
-    local unzigzaged = unzigzag(buffer(seqnr_pos,seqnr_maxlen))
-    local seqnr_len = unzigzaged:len() + 1
-    payload_tree:add_le(field_seqnr, unzigzaged)
+    local unzigzaged = unzigzag(buffer(seqnr_pos, seqnr_maxlen))
+    local seqnr_len = unzigzaged[1]
+    payload_tree:add_le(field_seqnr, unzigzaged[2])
 
+    local peer_id_pos = seqnr_pos + seqnr_len
+    local peer_id_maxlen = 3
+    local peer_id_unzigzaged = unzigzag(buffer(peer_id_pos, peer_id_maxlen))
+    local peer_id_len = peer_id_unzigzaged[1]
+    payload_tree:add_le(field_peer_id, peer_id_unzigzaged[2])
 
-    local msg_length_pos = seqnr_pos + seqnr_len
+    local msg_length_pos = peer_id_pos + peer_id_len
     local msg_length_maxlen = 8
     local msg_length_buffer = unzigzag(buffer(msg_length_pos, msg_length_maxlen))
-    payload_tree:add_le(field_msg_length, msg_length_buffer)
-    local msg_length_len = msg_length_buffer:len()
-
+    local msg_length_len = msg_length_buffer[1]
+    payload_tree:add_le(field_msg_length, msg_length_buffer[2])
 
     local ip_pos = msg_length_pos + msg_length_len
     local ip_len = buffer:len() - ip_pos
     local ip_buffer = buffer(ip_pos, ip_len)
-    
+
     --Dissector.get("eth_withoutfcs"):call(ip_buffer:tvb(), pinfo, tree)
     Dissector.get("ip"):call(ip_buffer:tvb(), pinfo, tree)
 end
